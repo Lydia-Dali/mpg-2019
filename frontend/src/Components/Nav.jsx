271c2bda85394b47de422d90b2408115e78606e8
Lydia-Dali <lydia.dali.wild@gmail.com>
2019-02-13 13:21:39 +0100

    Building Nav Component

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -4,28 +4,46 @@ import { NavLink } from "react-router-dom";
 export default class Nav extends Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid myContainerFluid">
+
+        {/* Navbar */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light nav">
+        <a className="navbar-brand" href="#">
         <h1>I am a nav</h1>
-        <ul>
+        </a>
+        <button aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" className="navbar-toggler" data-target="#navbarNav" data-toggle="collapse" type="button"><span className="navbar-toggler-icon"></span></button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
             <NavLink exact to="/" onClick="refreshPage()" activeClassName="selected">
-              <li className="active">Welcome</li>
+              <li className="active">
+              <a class="nav-link" href="#">
+              <h3>Welcome</h3><span class="sr-only">(current)</span></a></li>
             </NavLink>
             <NavLink exact to="/Agenda" activeClassName="selected">
-              <li>Agenda</li>
+              <li className="nav-item"><a class="nav-link" href="#agenda">
+                        <h3>Agenda</h3></a></li>
             </NavLink>
             <NavLink exact to="/Incontournable" activeClassName="selected">
-              <li>Incontournable</li>
+              <li className="nav-item"><a class="nav-link" href="#incontournable">
+                        <h3>Incontournable</h3></a></li>
             </NavLink>
             <NavLink exact to="/Gallerie">
-              <li>Gallerie</li>
+              <li className="nav-item"><a class="nav-link" href="#gallerie">
+                        <h3>Gallerie</h3></a></li>
             </NavLink>
             <NavLink exact to="/Presse">
-              <li>Presse</li>
+              <li className="nav-item"><a class="nav-link" href="#presse">
+                        <h3>Presse</h3></a></li>
             </NavLink>
             <NavLink exact to="/Contact">
-              <li>Contact</li>
+              <li className="nav-item"><a class="nav-link" href="#presse">
+                        <h3>Contact</h3></a></li>
             </NavLink>
           </ul>
+          </div>
+        </nav>
+
+
       </div>
     )
   }
