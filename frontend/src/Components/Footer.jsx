9950db0327302aab35276ddf85ae371cef9be915
Lydia-Dali <lydia.dali.wild@gmail.com>
2019-02-13 15:05:08 +0100

    Build Footer Component

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -4,26 +4,28 @@ import { Link } from "react-router-dom";
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        <ul>            
-            <Link className="sous-menu" to="/Contact">
-              <li>Contact</li>
-            </Link>
-            <Link className="sous-menu" to="/Presse">
-              <li>Presse</li>
-            </Link>
-            <Link className="sous-menu" to="/Help">
-              <li>Help</li>
-            </Link>
-            <Link className="sous-menu" to="/Faq">
-              <li>FAQ</li>
-            </Link>
-            <Link className="sous-menu" to="/Cgu">
-              <li>C.G.U</li>
-            </Link>
-            <h6>Powered by Lydia Dali / Code4Marseille</h6>
+      <div className="row footer margin_zero">
+        <p className="footer_title">RESTONS EN CONTACT</p>
+        <div className="container_social">
+          <ul className="socialIcons">
+            <li className="facebook"><a href="https://fr-fr.facebook.com/Voyageursdumonde/"><i className="fa fa-fw fa-facebook"></i>Facebook</a></li>
+            <li className="twitter"><a href="https://twitter.com/conseilsvoyages?lang=fr"><i className="fa fa-fw fa-twitter"></i>Twitter</a></li>
+            <li className="instagram"><a href="https://www.instagram.com/afrenchtraveler/"><i className="fa fa-fw fa-instagram"></i>Instagram</a></li>
+            <li className="pinterest"><a href="https://www.pinterest.fr/vvagabondages/"><i className="fa fa-fw fa-pinterest-p"></i>Pinterest</a></li>
           </ul>
-        
+        </div>
+        <ul>
+          <Link className="sous-menu" to="/Presse">
+            <li>Presse</li>
+          </Link>
+          <Link className="sous-menu" to="/Contact">
+            <li>Contact</li>
+          </Link>          
+          <Link className="sous-menu" to="/Cgu">
+            <li>Mentions Légales</li>
+          </Link>
+        </ul>
+        <p className="copyright">2019 Powered by Lydia Dali © MPG 2019 / Code4Marseille - TOUS DROITS RÉSERVÉS.</p>
       </div>
     )
   }
